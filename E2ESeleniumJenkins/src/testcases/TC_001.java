9edbf8d98408b388f403630907d275cfe5614667
pragypan <pragypan@LNAR-PF1ELTZG.corp.capgemini.com>
2019-02-20 15:21:28 -0500

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -0,0 +1,18 @@
+package testcases;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	 @Test
+	 public static void testcases()
+	 {
+		 System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+		 ChromeDriver driver = new ChromeDriver();
+		 driver.get("http://www.facebook.com");
+		 driver.findElementById("email").sendKeys("Hello");
+		 driver.findElementById("pass").sendKeys("Hello");
+		 driver.quit();
+	 }
+  
+}
